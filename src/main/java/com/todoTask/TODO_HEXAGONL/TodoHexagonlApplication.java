398ec2cc86d6e986223f255d83409543d0eb3860
Juan José Arenas G. <juanjoarenas1218@gmail.com>
2024-10-17 15:58:23 -0500

    initial

---
+++ src/main/java/com/todoTask/TODO_HEXAGONL/TodoHexagonlApplication.java
@@ -0,0 +1,13 @@
+package com.todoTask.TODO_HEXAGONL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoHexagonlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoHexagonlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/todoTask/TODO_HEXAGONL/TodoHexagonlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todoTask.TODO_HEXAGONL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoHexagonlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
