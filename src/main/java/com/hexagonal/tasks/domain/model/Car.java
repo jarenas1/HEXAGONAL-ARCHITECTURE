f1fed648c40a8c67a1982c22fd263b1d2d764d97
Juan José Arenas G. <juanjoarenas1218@gmail.com>
2025-03-11 18:38:32 -0500

    add: som validations in models

--- src/main/java/com/hexagonal/tasks/domain/model/Car.java
+++ src/main/java/com/hexagonal/tasks/domain/model/Car.java
@@ -3,15 +3,20 @@ package com.hexagonal.tasks.domain.model;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Data;
 
 @Builder
 @Data
 public class Car {
+    @NotNull
     private Long id;
+
+    @NotNull
     private String model;
 //    @ManyToOne
 //    @JoinColumn(name = "owner_id")
+    @NotNull
     private Person owner;
 }
--- src/main/java/com/hexagonal/tasks/domain/model/Person.java
+++ src/main/java/com/hexagonal/tasks/domain/model/Person.java
@@ -3,6 +3,8 @@ package com.hexagonal.tasks.domain.model;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Data;
 
@@ -11,8 +13,13 @@ import java.util.List;
 @Builder
 @Data
 public class Person {
+    @NotNull
     private Long id;
+
+    @NotNull
+    @Min(3)
     private String name;
 //    @OneToMany(mappedBy = "owner", cascade = CascadeType.ALL, orphanRemoval = true)
+    @NotNull
     private List<Car> cars;
 }
