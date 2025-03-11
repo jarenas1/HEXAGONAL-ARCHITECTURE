4e7c28a3d90938d5977777cf2206cda1a47e3842
Juan José Arenas G. <juanjoarenas1218@gmail.com>
2025-03-11 18:31:17 -0500

    create: some exceptions

---
+++ src/main/java/com/hexagonal/tasks/domain/exceptions/UserDoesntHaveCarsExceptions.java
@@ -0,0 +1,4 @@
+package com.hexagonal.tasks.domain.exceptions;
+
+public class UserDoesntHaveCarsExceptions extends RuntimeException {
+}
---
+++ src/main/java/com/hexagonal/tasks/domain/exceptions/UserNotFoundException.java
@@ -0,0 +1,4 @@
+package com.hexagonal.tasks.domain.exceptions;
+
+public class UserNotFoundException extends RuntimeException {
+}
